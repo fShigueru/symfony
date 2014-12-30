b4c0ae3cd4a420931a009de92fba62d5c3059f05
root <root@quantal64.(none)>
2014-12-30 00:36:32 +0000

    Primeiro commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/{nome}", name="homepage")
+     */
+    public function indexAction($nome)
+    {
+        return $this->render('AppBundle:Default:index.html.twig', ['nome' => $nome]);
+    }
+
+    /**
+     * @Route("/run/{marca}/{modelo}", name="homepage")
+     */
+    public function runAction($marca, $modelo)
+    {
+    	$carro['marca'] = $marca;
+    	$carro['modelo'] = $modelo;
+    	return $this->render('AppBundle:Default:run.html.twig', ['carro' => $carro]);
+    }
+}
