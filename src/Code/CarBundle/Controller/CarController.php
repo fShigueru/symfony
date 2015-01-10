dfaf88dd28449548f977359795d9682c5697ef6d
root <root@quantal64.(none)>
2015-01-10 00:30:54 +0000

    Lista de carros no controller CarController, listar na view index de Car

--- src/Code/CarBundle/Controller/CarController.php
+++ src/Code/CarBundle/Controller/CarController.php
@@ -9,16 +9,28 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class CarController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("")
      * @Template()
      */
     public function indexAction()
     {
-    	echo 'aa';
-    	exit;
-        return array(
-                // ...
-            );
+    	//lista de carros
+    	$cars[0]['marca'] = "GM";
+    	$cars[0]['modelo'] = "Cobalt";
+
+    	$cars[1]['marca'] = "Honda";
+    	$cars[1]['modelo'] = "City";
+
+    	$cars[2]['marca'] = "Jac Motors";
+    	$cars[2]['modelo'] = "J3 Turin";
+
+    	$cars[3]['marca'] = "Peugeot";
+    	$cars[3]['modelo'] = "207 Sedan";
+
+    	$cars[4]['marca'] = "Volkswagen";
+    	$cars[4]['modelo'] = "Polo Sedan";
+
+        return ['cars' => $cars];
     }
 
 }
