2ffc38c30aec2ce8544525ebae6c1c1148ebb219
root <root@packer-virtualbox-iso.vagrantup.com>
2015-01-22 00:21:05 +0000

    Ajustes home sistema

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,24 +4,28 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/{nome}", name="homepage")
+     * @Route("/", name="homepage")
+     * @Template()
      */
-    public function indexAction($nome)
+    public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig', ['nome' => $nome]);
+        //return $this->render('AppBundle:Default:index.html.twig', ['nome' => $nome]);
+        return [];
     }
 
     /**
      * @Route("/run/{marca}/{modelo}", name="runpage")
+     * @Template()
      */
     public function runAction($marca, $modelo)
     {
     	$carro['marca']  = $marca;
     	$carro['modelo'] = $modelo;
-    	return $this->render('AppBundle:Default:run.html.twig', ['carro' => $carro]);
+    	return ['carro' => $carro];
     }
 }
